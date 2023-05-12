d3d8294a71f7bcf503d238791cd2a44abd21a4a3
SQFMI <github@sqfmi.com>
2023-05-12 12:01:37 -0400

    Deploy website - based on 3978c4fe487a82033d2b11ce77b452fadea50226

---
+++ assets/js/8a3a63f7.06fe2fbd.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbeepberry=self.webpackChunkbeepberry||[]).push([[858],{6305:e=>{e.exports=JSON.parse('{"title":"Hardware","slug":"/category/hardware","permalink":"/docs/category/hardware","navigation":{"previous":{"title":"RGB LED","permalink":"/docs/firmware/rgb-led"},"next":{"title":"Pinouts","permalink":"/docs/hardware/pinouts"}}}')}}]);
